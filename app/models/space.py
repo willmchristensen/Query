5353a688c96aa759f20768458e173a6dccc75b9d
alexl <alexandermlugbiihl@gmail.com>
2023-05-05 19:02:45 -0400

    space seeder

--- app/models/space.py
+++ app/models/space.py
@@ -11,6 +11,7 @@ class Space(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255), nullable=False, unique=True)
     owner_id = db.Column(db.String, db.ForeignKey("users.id"), nullable=False)
+    image_url = db.Column(db.String)
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.now())
     updated_at = db.Column(db.DateTime)
 
@@ -24,6 +25,7 @@ class Space(db.Model):
             "name": self.name,
             "ownerId": self.owner_id,
             "questions": [question.to_dict() for question in self.questions],
+            "imageUrl": self.image_url,
             "createdAt": str(self.created_at),
             "updatedAt": str(self.updated_at)
         }
