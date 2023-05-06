da79b2af4b0b4634e4695c9f02cda394d50181cc
willmchristensen <johndoe@example.com>
2023-05-06 14:56:52 -0700

    added create a question form and route

--- app/api/questions_route.py
+++ app/api/questions_route.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template, redirect
-from app.models import Question, Answer
-from flask_login import login_required
+from app.models import Question, Answer, db
+from flask_login import login_required,current_user
+from app.forms import QuestionForm
 
 question_routes = Blueprint("questions", __name__)
 
@@ -27,3 +28,28 @@ def get_one_question(id):
     response["answers"] = [answer.to_dict() for answer in answers]
     print("question", response)
     return {'question': response}
+
+@question_routes.route('/new')
+@login_required
+def create_one_question():
+    """
+    Create a question     
+    """
+    form = QuestionForm()
+
+    if form.validate_on_submit():
+        data = form.data
+        new_question = Question(
+            details = data['details'],
+            user_id = current_user.id,
+        )
+    
+        db.session.add(new_question)
+        db.session.commit()
+        return {
+            "question": new_question
+        }
+
+    return {
+        "errors": form.errors
+    }
