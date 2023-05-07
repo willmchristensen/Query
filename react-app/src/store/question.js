40b0341b69889c487ddfe37a94ec84d69489bf0b
TheSicilian12 <mguidera314@gmail.com>
2023-05-07 16:44:34 -0400

    working on connecting get all questions to front end

--- react-app/src/store/question.js
+++ react-app/src/store/question.js
@@ -1,10 +1,29 @@
 const POST_QUESTION = "questions/new";
+const GET_ALL_QUESTIONS = "questions";
+
+const getAllQuestions = (questions) => ({
+    type: GET_ALL_QUESTIONS,
+    questions
+});
 
 const postQuestion = (details) => ({
     type: POST_QUESTION,
     details
 });
 
+
+export const retreiveAllQuestions = () => async (dispatch) => {
+    console.log('All Question THUNK')
+    const response = await fetch("/api/questions")
+    if (response.ok) {
+        const data = await response.json();
+        dispatch(getAllQuestions(data))
+    } else {
+        return [
+            "An error occurred. Please try again."
+        ];
+    }
+}
 
 export const createQuestion = (details) => async (dispatch) => {
     console.log('details in THUNK',details)
@@ -16,7 +35,7 @@ export const createQuestion = (details) => async (dispatch) => {
         body: details
         // body: JSON.stringify({
         //     details
-        // }), 
+        // }),
     });
     if (response.ok) {
         const data = await response.json();
@@ -38,10 +57,15 @@ const initialState = { questions: {} };
 export default function questionReducer(state = initialState, action) {
 	switch (action.type) {
 		case POST_QUESTION:
-            const newState = { ...state };
-            newState.questions[action.details.id] = action.details;
-            return newState;
+            const post_newState = { ...state };
+            post_newState.questions[action.details.id] = action.details;
+            return post_newState;
+        case GET_ALL_QUESTIONS:
+            const get_all_newState = { ...state };
+            get_all_newState.questions = action.question
+            return get_all_newState
 		default:
 			return state;
 	}
 }
+
