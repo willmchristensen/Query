8ee9db7e944bcad00bbb4a19173c2acc9272c2bb
TheSicilian12 <mguidera314@gmail.com>
2023-05-08 14:17:11 -0400

    thunk and reducer for post answer, not tested!

--- react-app/src/store/answer.js
+++ react-app/src/store/answer.js
@@ -1,11 +1,17 @@
 import normalize from './normalizer'
 
+const POST_ANSWER = "answers/new";
 const LOAD = "answers/load"
 
 const load = (data) => ({
     type: LOAD,
     payload: data
 })
+
+const postAnswer = (details) => ({
+    type: POST_ANSWER,
+    details
+});
 
 export const getAllAnswers = (userId) => async (dispatch) => {
     //This is the get all answers Thunk
@@ -16,6 +22,34 @@ export const getAllAnswers = (userId) => async (dispatch) => {
         const allAnswers = normalize(data.answers);
         dispatch(load(allAnswers))
         return response
+    }
+}
+
+export const createAnswer = (details) => async (dispatch) => {
+    //This is the create an answer Thunk
+    console.log('details in THUNK', details)
+    const response = await fetch("/api/answers/new", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        // body: details
+        body: JSON.stringify(
+            details
+        ),
+    });
+    if (response.ok) {
+        const data = await response.json();
+        dispatch(postAnswer(data));
+    } else if (response.status < 500) {
+        const data = await response.json();
+        if (data.errors) {
+            return data.errors;
+        }
+    } else {
+        return [
+            "An error occurred. Please try again."
+        ];
     }
 }
 
@@ -34,10 +68,11 @@ const answerReducer = (state = initialState, action) => {
         //     const single_newState = { ...state };
         //     single_newState.singleQuestion = {...action.payload};
         //     return single_newState;
-        // case POST_QUESTION:
-        //     const post_newState = { ...state };
-        //     post_newState.questions[action.details.question.id] = action.details.question;
-        //     return post_newState;
+        case POST_ANSWER:
+            // NOT DONE OR TESTED!
+            const post_newState = { ...state };
+            post_newState.answers[action.details.answer.id] = action.details.answer;
+            return post_newState;
         default:
             return state;
     }
