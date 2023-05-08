89a9bbae915a876a4301ca625e962a7ec4dd9d17
alexl <alexandermlugbiihl@gmail.com>
2023-05-08 13:22:10 -0400

    get all questions thunk, and normalizer

--- react-app/src/store/answer.js
+++ react-app/src/store/answer.js
@@ -1,5 +1,23 @@
+import normalize from './normalizer'
 
+const LOAD = "answers/load"
 
+const load = (data) => ({
+    type: LOAD,
+    payload: data
+})
+
+export const getAllAnswers = (userId) => async (dispatch) => {
+    //This is the get all answers Thunk
+    console.log("inside gett all answers thunk")
+    const response = await fetch(`/api/answers/${userId}`)
+    if (response.ok) {
+        const data = await response.json();
+        const allQuestions = normalize(data.questions);
+        dispatch(load(allQuestions))
+        return response
+    } else { }
+}
 
 
 const initialState = {
