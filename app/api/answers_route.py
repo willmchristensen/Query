59fe907218705ef4608882fa54fd0dac11793d94
TheSicilian12 <mguidera314@gmail.com>
2023-05-08 12:34:36 -0400

    all answers backend route, testing needed

---
+++ app/api/answers_route.py
@@ -0,0 +1,20 @@
+from flask import Blueprint, render_template, redirect, request
+from app.models import Question, Answer, db, User
+from flask_login import login_required
+# from app.forms import QuestionForm
+
+answer_routes = Blueprint("answers", __name__)
+
+# ALL ANSWERS BY USER ID
+@answer_routes.route('')
+def get_answer_routes(userId):
+    """
+    Query for all answers by user id and returns them in a list of dictionaries
+    """
+    # all_questions = Question.query.all()
+    # response = [question.to_dict() for question in all_questions]
+    # # print('get_all_questions response: ', response)
+    # return {'questions': response}
+    all_answers = Answer.query.filter(Answer.owner_id == userId).all()
+    response = [answer.to_dict() for answer in all_answers]
+    return {"answers": response}
