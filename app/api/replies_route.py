c76ec4a064291091e48730de0b6e0c3b49a5adea
TheSicilian12 <mguidera314@gmail.com>
2023-05-09 12:16:06 -0400

    save before major edit

--- app/api/replies_route.py
+++ app/api/replies_route.py
@@ -15,11 +15,11 @@ def get_reply_routes(answer_id):
     return {"reply": response}
 
 # Get all replies by user id
-@reply_routes.route('/user/<int:user_id>')
-def get_reply_routes(user_id):
-    """
-    Query for all replies by user id and returns them in a list of dictionaries
-    """
-    all_replies = Reply.query.filter(Reply.owner_id == user_id).all()
-    response = [reply.to_dict() for reply in all_replies]
-    return {"reply": response}
+# @reply_routes.route('/user/<int:user_id>')
+# def get_reply_routes(user_id):
+#     """
+#     Query for all replies by user id and returns them in a list of dictionaries
+#     """
+#     all_replies = Reply.query.filter(Reply.owner_id == user_id).all()
+#     response = [reply.to_dict() for reply in all_replies]
+#     return {"reply": response}
