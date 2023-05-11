5567bd00829caa1e44432f3b661498e400d16968
TheSicilian12 <mguidera314@gmail.com>
2023-05-11 19:37:42 -0400

    errors with background image

--- react-app/src/components/SplashPage/index.js
+++ react-app/src/components/SplashPage/index.js
@@ -3,9 +3,8 @@ import SignupFormPage from "../SignupFormPage"
 import { useDispatch, useSelector } from "react-redux";
 import { Redirect } from "react-router-dom";
 import { login } from "../../store/session";
-import splashBackground from "../../assets/images/splash-page-background.jpg"
+// import splashBackground from "../../assets/images/splash-page-background.jpg"
 import "./SplashPage.css"
-
 
 function SplashPage() {
     const dispatch = useDispatch();
@@ -19,27 +18,30 @@ function SplashPage() {
     }
 
     return (
-        <div className="splash-background">
-            <div className="splash-page-container white-background">
-                <h1 className="query quora-font">Query</h1>
-                <h2 className="mission-statement">A place to share knowledge and better understand the world</h2>
-                <div className="login-signup-container">
-                    <div className="width-fifty">
-                        <div className="signup-splash-page">
-                            <SignupFormPage />
+
+            <div className="splash-background">
+
+                <div className="splash-page-container white-background">
+                    <h1 className="query quora-font">Query</h1>
+                    <h2 className="mission-statement">A place to share knowledge and better understand the world</h2>
+                    <div className="login-signup-container">
+                        <div className="width-fifty">
+                            <div className="signup-splash-page">
+                                <SignupFormPage />
+                            </div>
                         </div>
-                    </div>
-                    <div className="width-fifty right-side">
-                        <div className="login-splash-page">
-                            <LoginFormPage />
-                            <div className="splash-page-demo-button">
-                                <button className="oval-button" onClick={handleClick}>Demo User</button>
+                        <div className="width-fifty right-side">
+                            <div className="login-splash-page">
+                                <LoginFormPage />
+                                <div className="splash-page-demo-button">
+                                    <button className="oval-button" onClick={handleClick}>Demo User</button>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
+
     )
 }
 
