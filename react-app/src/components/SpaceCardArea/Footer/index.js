2948ff18fa16f310605b2ea714c82fa1854f12c6
alexl <alexandermlugbiihl@gmail.com>
2023-06-08 17:09:40 -0400

    linkedin link and add comment styling

--- react-app/src/components/SpaceCardArea/Footer/index.js
+++ react-app/src/components/SpaceCardArea/Footer/index.js
@@ -3,24 +3,44 @@ import './Footer.css'
 
 const Footer = () => {
 
-    return(
+    return (
         <>
-        <div className="project-links-title-container">
-            <div className="project-repo-link">
-                <a href="https://github.com/TheSicilian12/Query" target="_blank">App Repository</a>
+            <div className="project-links-title-container">
+                <div className="project-repo-link">
+                    <a href="https://github.com/TheSicilian12/Query" rel="noopener noreferrer" target="_blank">App Repository</a>
+                </div>
+                <div className="project-github-links-title">
+                    Creator Github Profiles and LinkedIn
+                </div>
             </div>
-            <div className="project-github-links-title">
-                Creator Github Profiles
-            </div>
-        </div>
-        <div className="project-github-links-container">
-            <nav className='project-github-links'>
-                <a href="https://github.com/cstever0" target="_blank">Cory Stever</a>
-                <a href="https://github.com/TheSicilian12" target="_blank">Michael Guidera</a>
-                <a href="https://github.com/willmchristensen" target="_blank">William Christensen</a>
-                <a href="https://github.com/Alugibihl" target="_blank">Alex Lugibihl</a>
-            </nav>
-        </div>
+            <div className="project-github-links-container">
+                <nav className='project-github-links'>
+                    <div className='whole'> William Christensen
+                        <div className='links'>
+                            <a href="https://github.com/willmchristensen" rel="noopener noreferrer" target="_blank"><i class="fab fa-github"></i></a>
+                            <a href="https://www.linkedin.com/in/will-christensen-85531317a/" rel="noopener noreferrer" target="_blank"><i class="fab fa-linkedin"></i></a>
+                        </div>
+                    </div>
+                    <div className='whole'> Michael Guidera
+                        <div className='links'>
+                            <a href="https://github.com/TheSicilian12" rel="noopener noreferrer" target="_blank"><i class="fab fa-github"></i></a>
+                            <a href="https://www.linkedin.com/in/guidera-michael/" rel="noopener noreferrer" target="_blank"><i class="fab fa-linkedin"></i></a>
+                        </div>
+                    </div>
+                    <div className='whole'> Alexander Lugibihl
+                        <div className='links'>
+                            <a href="https://github.com/Alugibihl" rel="noopener noreferrer" target="_blank"><i class="fab fa-github"></i></a>
+                            <a href="https://www.linkedin.com/in/alexander-lugibihl/" rel="noopener noreferrer" target="_blank"><i class="fab fa-linkedin"></i></a>
+                        </div>
+                    </div>
+                    <div className='whole'> Cory <br />Stever
+                        <div className='links'>
+                            <a href="https://github.com/cstever0" rel="noopener noreferrer" target="_blank"><i class="fab fa-github"></i></a>
+                            <a href="https://www.linkedin.com/in/cory-stever-aa2730126/" rel="noopener noreferrer" target="_blank"><i class="fab fa-linkedin"></i></a>
+                        </div>
+                    </div>
+                </nav >
+            </div >
         </>
     )
 }
